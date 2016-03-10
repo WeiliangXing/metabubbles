162729a025ca63b4fd667c277e5e616c7728ce9f
wxing <xingwl0401@gmail.com>
2016-03-10 10:53:37 -0500

    service works for widtheight

--- app/circles.service.js
+++ app/circles.service.js
@@ -2,21 +2,19 @@ import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class Circles {
-  // circles = [
-  //   {x: 50, y: 50, radius: 10},
-  //   {x: 75, y: 75, radius: 20},
-  //   {x: 115, y: 115, radius: 30}
-  // ];
-  constructor() {
+
+  static parameters = ['canvasWidth', 'canvasHeight'];
+  constructor(canvasWidth, canvasHeight) {
     this.circles = [];
     for (let i=0 ; i<100 ; i++) {
       this.circles.push({
-        x: this.randInt(900), // 0..900
-        y: this.randInt(500), // 0..500
-        radius: this.randInt(100) + 10 // 10..110
+        x: this.randInt(canvasWidth),
+        y: this.randInt(canvasHeight),
+        radius: this.randInt(100) + 10
       });
     }
   }
+
   randInt(max) {
     return Math.floor(Math.random() * max);
   }
--- app/main.js
+++ app/main.js
@@ -1,4 +1,8 @@
+import {provide} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {CanvasComponent} from './canvas.component';
 
-bootstrap(CanvasComponent);
+bootstrap(CanvasComponent, [
+  provide('canvasWidth', {useValue: 900}),
+  provide('canvasHeight', {useValue: 500})
+]);
